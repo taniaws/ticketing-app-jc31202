d744d20ba06cda2a411c934918191148e37239be
baim1998 <najad16081998@gmail.com>
2024-08-07 11:31:08 +0700

    feat: create Navbar

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { Header } from '@/components/Header';
-import { Footer } from '@/components/Footer';
+//import { Header } from '@/components/Header';
+//import { Footer } from '@/components/Footer';
+import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,6 +13,7 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
+ 
   children,
 }: {
   children: React.ReactNode;
@@ -19,9 +21,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
+        <Navbar/>
         {children}
-        <Footer />
       </body>
     </html>
   );
