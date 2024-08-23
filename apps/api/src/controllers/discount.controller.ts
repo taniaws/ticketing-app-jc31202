57414dbc636a6ed8a7a56fc8ef5a6b7bcc428ed6
tania <natania.william09@gmail.com>
2024-08-23 11:12:40 +0700

    feat: redeem points and discounts

--- apps/api/src/controllers/discount.controller.ts
+++ apps/api/src/controllers/discount.controller.ts
@@ -123,4 +123,105 @@ export class DiscountController {
       console.log('Failed to mark expired discounts as deleted:', error);
     }
   }
+
+  //Redeem discount
+  async redeemDiscount(req: Request, res: Response, next: NextFunction) {
+    const { userId, discountCode, eventId, quantity } = req.body;
+
+    if (!userId || !discountCode || !eventId || !quantity) {
+      return res.status(400).json({
+        success: false,
+        message: 'User ID, discount code, event ID, and quantity are required',
+      });
+    }
+
+    try {
+      const discount = await prisma.discount.findUnique({
+        where: { code: discountCode },
+      });
+
+      if (!discount) {
+        return res.status(404).json({
+          success: false,
+          message: 'Discount code not found',
+        });
+      }
+
+      if (discount.isDeleted) {
+        return res.status(400).json({
+          success: false,
+          message: 'Discount code is invalid or expired',
+        });
+      }
+
+      const existingUse = await prisma.transaction.findFirst({
+        where: {
+          userId,
+          discountId: discount.id,
+          eventId,
+        },
+      });
+  
+
+      if (existingUse) {
+        return res.status(400).json({
+          success: false,
+          message: 'Discount code already used by user',
+        });
+      }
+
+      const event = await prisma.event.findUnique({
+        where: { id: eventId },
+      });
+  
+      if (!event) {
+        return res.status(404).json({
+          success: false,
+          message: 'Event not found',
+        });
+      }
+
+      const price = event.harga;
+      const discountAmount = (price * discount.percent) / 100;
+      const totalPrice = (price - discountAmount) * quantity;
+
+      const transaction = await prisma.transaction.create({
+        data: {
+          userId,
+          amount: totalPrice,
+          createdAt: new Date(),
+          eventId,
+          discountId: discount.id,
+          transactionCode: `TRANS_${new Date().getTime()}`,
+          quantity,
+          price,
+        },
+      });
+
+      await prisma.detailtransaction.create({
+        data: {
+          transactionId: transaction.id,
+          isAttendance: false,
+          ticketcode: `TICKET_${new Date().getTime()}`,
+        },
+      });
+
+      await prisma.discount.update({
+        where: { id: discount.id },
+        data: { isDeleted: true },
+      });
+
+      return res.status(200).json({
+        success: true,
+        message: 'Discount redeemed successfully',
+      });
+    } catch (error) {
+      console.error(error);
+      next({
+        success: false,
+        message: 'Failed to redeem discount',
+        error: error,
+      });
+    }
+  }
 }
